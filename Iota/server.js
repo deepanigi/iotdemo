0a927c6c0ddb1869ec6d483d709fb0a00c47e1f8
deepanigi <deepan.ignaatious@rojoconsultancy.com>
2018-02-12 01:29:04 +0100

    initial commit

--- Iota/server.js
+++ Iota/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
